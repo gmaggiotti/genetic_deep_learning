e62f64ead5236dc28da7ac524d65c1eba82a3d0d
gabe <gmaggiotti@gmail.com>
2019-01-03 12:28:01 -0300

    fix: always mate with first generation

--- src/GDL.py
+++ src/GDL.py
@@ -23,16 +23,16 @@ X = 2 * X / float(max) - 1
 # output, are the one-hot encoded labels
 y = np.loadtxt("../dataset/Y.txt", delimiter=",").reshape(X.__len__(), 1)
 
-epochs=6000
+epochs=600
 best_n_children = 4
 population_size = 10
 gen ={}
-generations = 3
+generations = 5
 
 
 ## Generate a poblation of neural networks each trained from a random starting weigth
 ## ordered by the best performers (low error)
-init_pob = [NN(X, y,epochs) for i in range(10)]
+init_pob = [NN(X, y,epochs) for i in range(population_size)]
 init_pob = sorted([(nn.get_error(), nn) for nn in init_pob])
 
 gen[0]=init_pob
@@ -42,8 +42,8 @@ for x in range(1,generations):
     population = []
     print("--- generation {} ---".format(x))
     for i in range(population_size):
-        candidate1 = init_pob[np.random.randint(best_n_children)][1].get_weight()
-        candidate2 = init_pob[np.random.randint(best_n_children)][1].get_weight()
+        candidate1 = gen[x-1][np.random.randint(best_n_children)][1].get_weight()
+        candidate2 = gen[x-1][np.random.randint(best_n_children)][1].get_weight()
         w_child = mate(candidate1, candidate2)
         aux = NN(X, y, epochs, w_child)
         population += [tuple((aux.get_error(), aux))]
@@ -51,4 +51,4 @@ for x in range(1,generations):
     del population
 
 print 'EoF'
-NN(X,y,6000)
+NN(X,y,3000)
