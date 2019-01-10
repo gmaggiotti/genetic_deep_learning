507c4ad17d6fcbe746078e19d36f320c0d803d61
gabe <gmaggiotti@gmail.com>
2019-01-10 15:26:14 -0300

    adding accuracy calculation

--- src/DNN_one_layer.py
+++ src/DNN_one_layer.py
@@ -29,6 +29,6 @@ def read_dataset():
 X, Y = read_dataset()
 train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.2, random_state=1)
 
-epochs = 60000
+epochs = 6000
 
 nn1 = NN1(train_x, train_y, test_x,test_y, epochs, print_step=600)
