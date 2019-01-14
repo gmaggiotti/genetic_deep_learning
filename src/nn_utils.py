bdfed2b7c0e7a790e52080a48ace790d999fea80
patelotech <pj.patriciojulian@gmail.com>
2019-01-15 00:45:10 +0100

    add test script

--- src/nn_utils.py
+++ src/nn_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from src.nn_utils import sigmoid
 
 
 def enum(**enums):
---
+++ test.py
@@ -0,0 +1,15 @@
+import pytest
+from src.GDNN_one_layer import gdnn
+import numpy as np
+from src.NN1 import NN1
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from src.nn_utils import mate, Type, sort_by_fittest
+
+
+dbpath = "/../dataset/data-500.csv"
+X, Y = gdnn.read_dataset(dbpath, 500)
+print(X, Y)
+result = gdnn.process(X, Y)
+
+print(result)
