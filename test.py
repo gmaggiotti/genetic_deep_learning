8eeeca6522a5780e39f9ad2ce1b149d94ac24004
patelotech <pj.patriciojulian@gmail.com>
2019-01-15 00:50:13 +0100

    f

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import pytest
-from src.GDNN_one_layer import gdnn
+from .src.GDNN_one_layer import gdnn
 import numpy as np
-from src.NN1 import NN1
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-from src.nn_utils import mate, Type, sort_by_fittest
 
 
 dbpath = "/../dataset/data-500.csv"
