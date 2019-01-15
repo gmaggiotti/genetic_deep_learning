261db432c95a4ee19cf2e97c6dc3e3e1c57c6f01
patelotech <pj.patriciojulian@gmail.com>
2019-01-15 01:06:46 +0100

    fix instance error

--- src/test.py
+++ src/test.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
+gdnn = gdnn()
 
 dbpath = "/../dataset/data-500.csv"
 X, Y = gdnn.read_dataset(dbpath, 500)
